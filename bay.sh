7e38491c4b923a9d6b0b684d7c0d41bc9531cc6a
codidsprit <iyamroshan0@gmail.com>
2023-01-20 03:03:52 +0545

    feat: install script

--- bay.sh
+++ bay.sh
@@ -20,7 +20,7 @@ echo "export ZDOTDIR=\"\$HOME\"/.config/zsh" | sudo tee -a /etc/zsh/zshenv
 touch $HOME/.cache/zsh/history
 
 # Clones my dotfiles and puts them in .config
-git clone --depth 1 --separate-git-dir=$HOME/.config/.dots https://gitlab.com/samdenton/dots.git tmpdots
+git clone --depth 1 --separate-git-dir=$HOME/.config/.dots https://gitlab.com/codedsprit/dotfiles.git tmpdots
 rsync --recursive --verbose --exclude '.git' tmpdots/ $HOME/
 rm -rf tmpdots
 /usr/bin/git --git-dir=$HOME/.config/.dots --work-tree=$HOME config --local status.showUntrackedFiles no
